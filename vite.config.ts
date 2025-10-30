6cf10187043af99a5f27d2e7fe85a38d4bb245fd
Tuan Hung Nguyen <tuanhungg1995@gmail.com>
2025-10-30 11:46:41 +0700

    add components structure and path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,13 @@ export default defineConfig({
       plugins: [['babel-plugin-react-compiler', { target: '19' }]],
     },
   })],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
