9a32a2587c0efb769229e862246917da4b6c553c
Tuan Hung Nguyen <tuanhungg1995@gmail.com>
2025-11-07 15:55:30 +0700

    update some file for group title

--- src/pages/Board/BoardContent/Groups/GroupTitle/GroupTitle.tsx
+++ src/pages/Board/BoardContent/Groups/GroupTitle/GroupTitle.tsx
@@ -1,17 +1,51 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+import CollapseGroup from "./CollapseGroup/CollapseGroup";
 
 interface GroupTitleProps {
     groupName: string;
 }
 
-const GroupTitle = ({groupName}: GroupTitleProps) => {
+const GroupTitle = ({ groupName }: GroupTitleProps) => {
 
     return (
-        <Box sx={{ p: 2, bgcolor: "grey.800", borderBottom: "1px solid", borderColor: "grey.700" }}>
-            <Typography variant="h6" sx={{ fontWeight: "bold", color: "grey.200" }}>
-                {groupName}
-            </Typography>
+        <Box
+            sx={{
+                display: 'flex',
+                height: '40px',
+                alignItems: 'center',
+                px: '8px',
+            }}
+        >
+            <CollapseGroup GroupTitleColor="blue" />
+            <TextField
+                variant="standard"
+                value={groupName}
+                InputProps={{
+                    disableUnderline: true,
+                    sx: {
+                        px: "8px",
+                        mb: "8px",
+                        width: "100%",
+                        color: "white",
+                        '& .MuiInputBase-input': {
+                            color: 'white',
+                            fontSize: '16px',
+                        },
+                        "&:focus-within": {
+                            borderRadius: "4px",
+                            border: "1px solid transparent",
+                            boxShadow: "0 0 0 2px #3b82f6",
+                        },
+                        "&:hover": {
+                            borderRadius: "4px",
+                            border: "1px solid transparent",
+                            boxShadow: "0 0 0 2px #3b82f6",
+                        },
+                    }
+                }}
+            />
         </Box>
     )
 
