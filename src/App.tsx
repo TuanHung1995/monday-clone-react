903df28bf086afa77922bebac0db4cd9e434d9a1
Tuan Hung Nguyen <tuanhungg1995@gmail.com>
2025-11-19 16:19:02 +0700

    config: reconfig path for vite and ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import Box from '@mui/material/Box'
-// import ModeSelect from '@components/common/Modal/ModeSelect/ModeSelect'
-import { BrowserRouter } from "react-router-dom";
-import AppLayout from "./layouts/AppLayout";
 import AppRouter from '@routes/AppRoutes'
 import './index.css'
 import { RouterProvider } from "react-router-dom";
--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,14 +1,14 @@
 import Box from "@mui/material/Box";
 import NavbarHome from "@pages/Board/BoardNav";
 import TaskGroups from "@pages/Board/BoardContent/Groups";
-import { mockGroups, mockColumns } from "@api/mock-board-data-1.ts";
+import { mockGroups, mockColumns } from "@apis/mock-board-data-1.ts";
 
 const BoardPage = () => {
   return (
-      <Box>
-        <NavbarHome />
-        <TaskGroups initialGroups={mockGroups} initialColumns={mockColumns} />
-      </Box>
+    <Box>
+      <NavbarHome />
+      <TaskGroups initialGroups={mockGroups} initialColumns={mockColumns} />
+    </Box>
   );
 }
 
